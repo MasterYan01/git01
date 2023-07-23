a0f5a945a805be0c166bfbec2e166d1507591de9
MasterYan01 <105603565+MasterYan01@users.noreply.github.com>
2023-07-23 23:18:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-哈哈彥銘仔
 ming =() => {
   return 彥銘被提
 }
 console.log(ming);
 
 
-O.O~~~i love u ^.^
-dear ming 
-ccc happy new year
-shit Taiwan Government 破政府
+O.O~~~i love u ^.^a
+
+shit Taiwan Government 破政府 彥銘＆街友
