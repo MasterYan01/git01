ece775850318fbe3de59d635071c02c10a3cc299
您的姓名 <105603565+MasterYan01@users.noreply.github.com>
2023-08-03 22:31:03 +0800

    刪除123

---
+++ hello 2.js
@@ -0,0 +1,10 @@
+const hello="hello"
+const hello1="hello1"
+const hello2="hello2"
+//hello2
+//hello3
+//hello4
+//hello5
+//hello4
+//hello6
+
---
+++ hey 2.js
@@ -0,0 +1,6 @@
+const hey ="hey"
+const hey1 ="hey1"
+const hey2 ="hey2"
+//const hey2 ="hey2"
+//hello4
+//hello5
