7dbe39e91fc4cd2f1adc38ccdd8db60910c6bdca
您的姓名 <105603565+MasterYan01@users.noreply.github.com>
2023-08-05 11:11:24 +0800

    siri

---
+++ siri.js
@@ -0,0 +1,24 @@
+ming =() => {
+  return 彥銘被提
+}
+let hi ="hi"
+console.log(ming);
+console.log(ming);
+console.log(hi);
+
+
+//O.O~~~i love u ^.^a
+//hello4
+//O.O~~~i love u ^.^a
+//幹幹幹幹幹幹幹幹幹幹幹幹幹幹幹幹幹
+//盧彥銘
+//盧彥銘
+//盧彥銘
+//盧彥銘
+//shit Taiwan Government 破政府 彥銘＆街友
+//shit Taiwan Government 破政府 彥銘＆街友
+
+//hello5
+
+//shit Taiwan Government 破政府 彥銘＆街友
+//O.O~~~i love u ^.^a
