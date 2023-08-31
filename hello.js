9bab42ae95a6a4feb51d83987d1665f050e69157
您的姓名 <105603565+MasterYan01@users.noreply.github.com>
2023-08-31 21:18:26 +0800

    commit831

--- hello.js
+++ hello.js
@@ -1,11 +1,9 @@
-const hello="hello"
-const hello1="hello1"
-const hello2="hello2"
-//hello2
-//hello3
-//hello4
-//hello5
-//hello4
-//hello6
-//hello7
-qqq
+const fs = require('fs')
+//console.log(fs);
+fs.writeFile('./座右銘.txt','三人行,則有我師焉', err =>{
+    if(err){
+        console.log("寫入失敗");
+        return
+    }
+    console.log("寫入成功");
+})
